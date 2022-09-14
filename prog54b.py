5ae947aaf609695bc6dd7275cfc819a80fed7078
Killian Lazcano <112727097+KillianLazcano@users.noreply.github.com>
2022-09-14 19:38:11 +0000

    update

--- prog54b.py
+++ prog54b.py
@@ -1,1 +1,14 @@
-first = float(input(475)
+length = int(input("Please enter length: "))
+width = int(input("Please enter width: "))
+height = int(input("Please enter length: "))
+volume = int(input("Please enter width: "))
+
+area = length + width + volume + height
+perim = area / 4
+print("the length is", length)
+print("the width is", width)
+print("the height is", height)
+print("the volume is", volume)
+
+print("\nThe area is", area)
+print("the perimeter is", perim)
