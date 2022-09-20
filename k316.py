9cf5faec4cf88382a9f8f74ee63c211c7811136f
Killian Lazcano <112727097+KillianLazcano@users.noreply.github.com>
2022-09-20 19:22:43 +0000

    update

---
+++ k316.py
@@ -0,0 +1,166 @@
+from stanfordkarel import *
+
+
+class ktools:
+  def m(self):
+    """Shorthand for Move"""
+    move()
+    
+  def tl(self):
+    """turn left"""
+    turn_left()
+
+  def tr(self):
+    """Turn Right"""
+    self.tl()
+    self.tl()
+    self.tl()
+
+  def ta(self):
+    """turn around"""
+    self.tl()
+    self.tl()
+
+  def pick(self):
+   """pick beeper"""
+   pick_beeper()
+
+  def put(self):
+    """put beeper"""
+    put_beeper()
+    
+  def put2(self):
+    """put two beepers in line"""
+    self.put()
+    self.m()
+    self.put()
+
+  def put5(self):
+    """put 5 beepers in line"""
+    self.put2()
+    self.m()
+    self.put2()
+    self.m()
+    self.put()
+
+  def h(self):
+    """print H with beepers"""
+    self.tl()
+    self.put5()
+    self.tr()
+    self.m()
+    self.m()
+    self.m()
+    self.tr()
+    self.put5()
+    self.ta()
+    self.m()
+    self.m()
+    self.tl()
+    self.m()
+    self.put2()
+    self.tl()
+    self.m()
+    self.m()
+    self.tl()
+    self.m()
+    self.m()
+    self.m()
+    self.m()
+
+    def e(self):
+     self.tl()
+    self.put5()
+    self.tr()
+    self.m()
+    self.put2()
+    self.tr()
+    self.m()
+    self.m()
+    self.tr()
+    self.put2()
+    self.tl()
+    self.m()
+    self.m()
+    self.tl()
+    self.put2()
+    self.m()
+    self.m()
+
+  def l(self):
+    """print l """
+    self.tl()
+    self.put5()
+    self.ta()
+    self.m()
+    self.m()
+    self.m()
+    self.m()
+    self.tl()
+    self.m()
+    self.put2()
+    self.m()
+    self.m()
+    
+  def o(self):
+    """print O with beepers"""
+    self.tl()
+    self.put5()
+    self.tr()
+    self.m()
+    self.put2()
+    self.m()
+    self.tr()
+    self.put5()
+    self.tr()
+    self.m()
+    self.put2()
+    self.ta()
+    self.m()
+    self.m()
+    self.m()
+
+    
+
+def main():
+    """ Karel code goes here! """
+    kt = ktools()
+    kt.m()
+    kt.m()
+    kt.tl()
+    kt.m()
+    kt.m()
+    kt.tr()
+    pick_beeper()
+    kt.m()
+    pick_beeper()
+    kt.m()
+    pick_beeper()
+    kt.m()
+    pick_beeper()
+    kt.tl()
+    kt.m()
+    pick_beeper()
+    kt.m()
+    pick_beeper()
+    kt.m()
+    pick_beeper()
+    kt.tl()
+    kt.m()
+    pick_beeper()
+    kt.m()
+    pick_beeper()
+    kt.m()
+    pick_beeper()
+    kt.tl()
+    kt.m()
+    pick_beeper()
+    kt.m()
+    pick_beeper()
+    kt.m()
+    kt.tl()
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
