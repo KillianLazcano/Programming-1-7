7ba1ad5a9776074b8077b56832b83d0645a23a8d
Killian Lazcano <112727097+KillianLazcano@users.noreply.github.com>
2022-11-28 20:00:10 +0000

    update

---
+++ prog52aclass.py
@@ -0,0 +1,35 @@
+class Shape:
+  def __init__(self, length, width):
+    self.length = length
+    self.width = width
+    self.area = 0
+    self.perim = 0
+
+  def calc(self):
+    self.area = self.length * self.width
+    self.perim = 2 * self.length + 2 * self.width
+
+  def getArea(self):
+    return self.area
+
+  def getperim(self):
+    return self.perim
+
+
+
+def main():
+  len = int(input("Enter length: "))
+  wid = int(input("Enter width: "))
+  shape = Shape(len, wid)
+  shape.calc()
+  area = shape.getArea()
+  perim = Shape.getperim()
+  print("Area", area)
+  print("Perimeter", perim)
+  
+
+  pass
+
+
+if __name__ == "__main__":
+  main()
